01d062f72983ce058a9c0bc226db9f565e65de19
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2025-08-28 13:39:31 +0500

    Complete main functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container');
+function createGrid(count) {
+  if (count && count > 0 && count <= 100) {
+    for (let i = 0; i < count * count; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      const countPerRow = count;
+      const containerWidth = container.clientWidth;
+      const size = (containerWidth / countPerRow);
+      console.log(containerWidth / countPerRow, (countPerRow - 1) * 5);
+
+      square.style.width = size + 'px';
+      square.style.height = size + 'px';
+
+      square.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = 'blue';
+      });
+      square.addEventListener('mouseleave', (e) => {
+        e.target.style.backgroundColor = 'green';
+      });
+
+      container.appendChild(square);
+    }
+  }
+}
+
+createGrid(50)
